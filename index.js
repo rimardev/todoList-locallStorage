7fe8aa4b7cdc1ecd92e17cf30ba29461ed0732e0
rimardev <catamarce.bs@gmail.com>
2025-08-07 17:39:13 -0400

    agrego renderizacion de tareas

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
 const input = document.getElementById('entradaTarea');
 const boton = document.getElementById('btnAgregar');
+const tabla = document.getElementById('tabla');
 
 //trae el Array del local storage || o crea uno vacio
 let arrayTareas = JSON.parse(localStorage.getItem('tareas')) || [];
+
+renderizarTareas();
+
+function renderizarTareas() {
+  let html = "";
+  for(let item of arrayTareas){
+    html += `<tr>
+      <td>${item.tarea}</td>
+      <td>${item.estado}</td>
+      <td>
+        <button>X</button>
+      </td>
+    </tr>`    
+  };
+  tabla.innerHTML = html;  
+};
+
+
 
 
 function agregarTarea(dataTarea) {
@@ -15,6 +34,8 @@ function agregarTarea(dataTarea) {
 
   //almacena Array en local storage
   localStorage.setItem('tareas', JSON.stringify(arrayTareas));
+  renderizarTareas();
+  input.value = "";
 };
 
 boton.addEventListener('click', () => {
