7dc0e30f1f38da73bb384d5cb190f409c197d8f0
rimardev <catamarce.bs@gmail.com>
2025-08-10 01:23:35 -0400

    se agrega id a los objetos

--- index.js
+++ index.js
@@ -1,44 +1,73 @@
 const input = document.getElementById('entradaTarea');
-const boton = document.getElementById('btnAgregar');
+
 const tabla = document.getElementById('tabla');
+
+let id = 0;
 
 //trae el Array del local storage || o crea uno vacio
 let arrayTareas = JSON.parse(localStorage.getItem('tareas')) || [];
 
-renderizarTareas();
+function almacenarTareas(arrayTareas) {
+   localStorage.setItem('tareas', JSON.stringify(arrayTareas));  
+};
 
 function renderizarTareas() {
   let html = "";
   for(let item of arrayTareas){
-    html += `<tr>
+    html += `
+    <tr>
       <td>${item.tarea}</td>
       <td>${item.estado}</td>
       <td>
-        <button>X</button>
+        <button id="${item.id}" class="btnBorrar">X</button>
       </td>
     </tr>`    
   };
-  tabla.innerHTML = html;  
+  tabla.innerHTML = html;
+  
+  input.value = "";  
 };
 
-
-
-
 function agregarTarea(dataTarea) {
+
+  if(arrayTareas.length == 0){
+  id = 1;
+  } else {
+  id = arrayTareas[0].id + 1;
+  }
+  //console.log(id)
+
   let data = {
+    id: id,
     tarea: dataTarea,
     estado: false
   };
-  arrayTareas.push(data);
-  console.log(arrayTareas);
-
-  //almacena Array en local storage
-  localStorage.setItem('tareas', JSON.stringify(arrayTareas));
+  arrayTareas.unshift(data);
+  //console.log(arrayTareas);
+  almacenarTareas(arrayTareas)
   renderizarTareas();
-  input.value = "";
 };
 
-boton.addEventListener('click', () => {
-  agregarTarea(input.value);
+function eliminarTarea(id) {
+  console.log("borrar " + id);  
+}
 
+function cambiarEstado(params) {
+  
+}
+
+renderizarTareas();
+
+//Esperando acciones *******************************
+const btnAdd = document.getElementById('btnAgregar');
+btnAdd.addEventListener('click', () => {
+  agregarTarea(input.value);
+});
+
+const borrar = document.querySelectorAll('.btnBorrar');
+borrar.forEach(boton => {
+  boton.addEventListener('click', () => {
+    //eliminarTarea(boton.id);
+    console.log("borrar " + boton.id);
+  });
 });
