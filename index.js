6b950364b72f96c9c3a8850877f495282a7e9162
rimardev <catamarce.bs@gmail.com>
2025-08-12 12:13:47 -0400

    agrego logica para eliminacion de tareas

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const input = document.getElementById('entradaTarea');
-
 const tabla = document.getElementById('tabla');
-
+let borrar = document.querySelectorAll('.btnBorrar');
 let id = 0;
 
 //trae el Array del local storage || o crea uno vacio
 let arrayTareas = JSON.parse(localStorage.getItem('tareas')) || [];
 
 function almacenarTareas(arrayTareas) {
-   localStorage.setItem('tareas', JSON.stringify(arrayTareas));  
+  localStorage.setItem('tareas', JSON.stringify(arrayTareas));
+  renderizarTareas();   
 };
 
 function renderizarTareas() {
@@ -23,51 +23,59 @@ function renderizarTareas() {
       </td>
     </tr>`    
   };
-  tabla.innerHTML = html;
-  
-  input.value = "";  
+  tabla.innerHTML = html;  
+  input.value = "";
+  //asigna escuchador eventos a botones borrar renderizados  
+  asignarEventosBorrar();
 };
 
 function agregarTarea(dataTarea) {
-
+  //crea id incremental a partir del existente.
   if(arrayTareas.length == 0){
   id = 1;
   } else {
   id = arrayTareas[0].id + 1;
   }
-  //console.log(id)
-
+  //crea Objeto tarea para guardar.
   let data = {
     id: id,
     tarea: dataTarea,
     estado: false
   };
+  //agrega Objeto tarea al array.
   arrayTareas.unshift(data);
-  //console.log(arrayTareas);
   almacenarTareas(arrayTareas)
-  renderizarTareas();
 };
 
 function eliminarTarea(id) {
-  console.log("borrar " + id);  
+  console.log(id);
+  let index = arrayTareas.findIndex(item => item.id == id);
+  console.log(index);
+  if (index !== -1) {
+    arrayTareas.splice(index, 1);
+  }
+  console.log(arrayTareas);
+  almacenarTareas(arrayTareas);
+};
+
+function asignarEventosBorrar() {
+  borrar = document.querySelectorAll('.btnBorrar');
+  borrar.forEach(boton => {
+    //evento borrar tarea.
+    boton.addEventListener('click', () => {
+      console.log("click en " + boton.id);
+      eliminarTarea(boton.id);
+    });  
+  });  
 }
 
-function cambiarEstado(params) {
-  
-}
+renderizarTareas();//Renderizacion Inicial
 
-renderizarTareas();
-
-//Esperando acciones *******************************
+//Evento agregar tarea.
 const btnAdd = document.getElementById('btnAgregar');
 btnAdd.addEventListener('click', () => {
   agregarTarea(input.value);
 });
 
-const borrar = document.querySelectorAll('.btnBorrar');
-borrar.forEach(boton => {
-  boton.addEventListener('click', () => {
-    //eliminarTarea(boton.id);
-    console.log("borrar " + boton.id);
-  });
-});
+
+
